8e04d4221f1a96ec9d2692dd82c418f95cedbf37
jubel-han <hanbingxin49@gmail.com>
2016-06-07 00:39:46 +0800

    C:1.6 Building list and detail views

--- dye/urls.py
+++ dye/urls.py
@@ -9,4 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls',
+                           namespace='blog',
+                           app_name='blog')),
 )
