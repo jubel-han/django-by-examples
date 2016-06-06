c56fe461eca213c6452815edf2920b1514eca27a
jubel-han <hanbingxin49@gmail.com>
2016-06-06 13:01:41 +0800

    C:1.2 Creating your first project

---
+++ dye/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for dye project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dye.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
