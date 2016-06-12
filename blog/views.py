7716a7cd998e8abecfee3beb306be7b009659375
jubel-han <hanbingxin49@gmail.com>
2016-06-12 17:23:21 +0800

    C:3.4.1 Creating a search view

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,9 @@ from django.core.mail import send_mail
 from django.db.models import Count
 from django.conf import settings
 from taggit.models import Tag
+from haystack.query import SearchQuerySet
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 class PostListView(ListView):
@@ -86,7 +87,7 @@ def post_share(request, post_id):
     # Retrieve post by id.
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
-
+    cd = None
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
@@ -106,4 +107,24 @@ def post_share(request, post_id):
 
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
-                                                    'sent': sent})
+                                                    'sent': sent,
+                                                    'cd': cd})
+
+
+def post_search(request):
+    form = SearchForm()
+    cd = results = total_results = None
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            cd = form.cleaned_data
+            results = SearchQuerySet().models(Post)\
+                                      .filter(content=cd['query']).load_all()
+            # count total results
+            total_results = results.count()
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'cd': cd,
+                   'results': results,
+                   'total_results': total_results})
