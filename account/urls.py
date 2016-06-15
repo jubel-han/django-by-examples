b28bc9f652ad77d4fb147b868c6e93768402340e
jubel-han <hanbingxin49@gmail.com>
2016-06-15 22:57:35 +0800

    C:4.4 Chang password views

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import logout
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -10,6 +10,10 @@ urlpatterns = [
     url('^$', view=views.dashboard, name='dashboard'),
     url(r'^login/$', view='django.contrib.auth.views.login', name='login'),
     # url(r'^logout/$', view='django.contrib.auth.views.logout', name='logout'),
-    url(r'^logout/$', logout, {'template_name': 'registration/logout.html'}, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logout.html'}, name='logout'),
     url(r'^logout-then-login/$', view='django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+    url(r'^password-change/$', auth_views.password_change,
+        {'template_name': 'registration/password_change.html'}, name='password_change'),
+    url(r'^password-change/done/$', auth_views.password_change_done,
+        {'template_name': 'registration/password_change_success.html'}, name='password_change_done'),
 ]
