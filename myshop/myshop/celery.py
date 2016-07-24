bb8b99965ede4937ed33fe7eb6fffc8bcb26a2cc
jubel-han <hanbingxin49@gmail.com>
2016-07-24 17:44:57 +0800

    :bug: fix celery module import error

--- myshop/myshop/celery.py
+++ myshop/myshop/celery.py
@@ -1,7 +1,7 @@
+from __future__ import absolute_import
 import os
-from celery import Celery
 from django.conf import settings
-
+from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
