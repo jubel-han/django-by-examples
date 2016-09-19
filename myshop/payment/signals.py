5a5c45ce1f08e57525cc2e6dd434ba4c6f139742
jubel <hanbingxin49@gmail.com>
2016-09-19 16:55:17 +0800

    C:8.1.7 Sending PDF files by e-mail

--- myshop/payment/signals.py
+++ myshop/payment/signals.py
@@ -1,7 +1,12 @@
 from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 from paypal.standard.models import ST_PP_COMPLETED
 from paypal.standard.ipn.signals import valid_ipn_received
 from orders.models import Order
+from django.core.mail import EmailMessage
+from django.conf import settings
+from io import BytesIO
+import weasyprint
 
 
 def payment_notification(sender, **kwargs):
@@ -12,5 +17,22 @@ def payment_notification(sender, **kwargs):
         # mark the order as paid
         order.paid = True
         order.save()
+
+        # create invoice e-mail
+        subject = 'My Shop - Invoice no.{}'.format(order.id)
+        message = 'Please, find attached the invoice for you recent purchase.'
+        email = EmailMessage(subject, message, 'admin@myshop.com', [order.email])
+
+        # generte PDF
+        html = render_to_string('orders/order/pdf.html', {'order': order})
+        out = BytesIO()
+        stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+        weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+
+        # attach PDF file
+        email.attach('order_{}.pdf'.format(order.id), out.getvalue(), 'application/pdf')
+
+        # send email
+        email.send()
 
 valid_ipn_received.connect(payment_notification)
