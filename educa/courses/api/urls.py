14d43e8b4364363d0174c740cc74c956d987d0e4
jubel <hanbingxin49@gmail.com>
2017-01-01 15:23:14 +0800

    C:12.6 Building custom views

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'subjects/$', views.SubjectListView.as_view(), name='subject_list'),
     url(r'subjects/(?P<pk>\d+)/$', views.SubjectDetailView.as_view(), name='suject_detail'),
+    url(r'courses/(?P<pk>\d+)/enroll/$', views.CourseEnrollView.as_view(), name='course_enroll'),
 ]
