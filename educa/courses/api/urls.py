1f661ab53fde544b12179fc0f37daf100b782a26
jubel <hanbingxin49@gmail.com>
2017-01-01 15:56:12 +0800

    C:12.9 Adding additional actions to view sets

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -8,6 +8,5 @@ router.register('courses', views.CourseViewSet)
 urlpatterns = [
     url(r'subjects/$', views.SubjectListView.as_view(), name='subject_list'),
     url(r'subjects/(?P<pk>\d+)/$', views.SubjectDetailView.as_view(), name='suject_detail'),
-    url(r'courses/(?P<pk>\d+)/enroll/$', views.CourseEnrollView.as_view(), name='course_enroll'),
     url(r'^', include(router.urls)),
 ]
