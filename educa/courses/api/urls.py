88a579934d9fb56a5a1ee73c5fce817ef3e9c21a
jubel <hanbingxin49@gmail.com>
2017-01-01 15:46:55 +0800

    C:11.8 Creating view sets and routers

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
+from django.conf.urls import url, include
 from . import views
 
+router = DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 urlpatterns = [
     url(r'subjects/$', views.SubjectListView.as_view(), name='subject_list'),
     url(r'subjects/(?P<pk>\d+)/$', views.SubjectDetailView.as_view(), name='suject_detail'),
     url(r'courses/(?P<pk>\d+)/enroll/$', views.CourseEnrollView.as_view(), name='course_enroll'),
+    url(r'^', include(router.urls)),
 ]
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from ..models import Subject, Course
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -19,8 +21,14 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 class CourseEnrollView(APIView):
     authentication_classes = (BasicAuthentication, )
+    permission_classes = (IsAuthenticated, )
 
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': 'True'})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
